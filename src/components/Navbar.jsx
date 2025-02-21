16c8f2e5f51b28a188e4f1ecdf4c77f13364f1ed
JamesDev292 <james.barthee@orange.fr>
2025-02-21 16:39:41 +0100

    add research navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,48 @@
-import React from 'react';
-import { Button } from "@chakra-ui/react"
+import React, { useState } from 'react';
+import { Button, IconButton, Input, Box } from "@chakra-ui/react"
 import { Link as RouterLink } from 'react-router-dom';
+import { LuSearch } from "react-icons/lu"
 import './Navbar.css'
 
 
 const Navbar = () => {
+  const [showSearch, setShowSearch] = useState(false);
+
+  const handleSearchToggle = () => {
+    setShowSearch((prev) => !prev);
+  };
 
   return (
     <nav className='navbarStyle'>
-      
-      <Button variant="ghost" size="sm" as={RouterLink} to="/" colorScheme="teal">Home</Button>
-      <Button variant="ghost" size="sm" as={RouterLink} to="/about" colorScheme="teal">About</Button>
-      <Button variant="ghost" size="sm" as={RouterLink} to="/dashboard" colorScheme="teal">Dashboard</Button>
-      <Button variant="ghost" size="sm">Click me</Button>
+      <div className='containerNavbarRight'>
+        <Button variant="ghost" size="sm" as={RouterLink} to="/" colorScheme="teal">Home</Button>
+        <Button variant="ghost" size="sm" as={RouterLink} to="/about" colorScheme="teal">About</Button>
+        <Button variant="ghost" size="sm" as={RouterLink} to="/dashboard" colorScheme="teal">Dashboard</Button>
+        <Button variant="ghost" size="sm">Click me</Button>
+      </div>
 
+      <div className='containerNavbarLeft' >
+          
+          <Box
+            overflow="hidden"
+            transition="max-width 0.3s ease"
+            maxW={showSearch ? '200px' : '0px'}
+          >
+            <Input
+              placeholder="Rechercher..."
+              size="sm"
+              borderRadius="md"
+              variant="outline"
+            />
+          </Box>
+
+        <IconButton aria-label="Search database" variant="ghost" size="sm" onClick={handleSearchToggle}>
+          <LuSearch />
+        </IconButton>
+
+
+
+      </div>
     </nav>
   );
 };
